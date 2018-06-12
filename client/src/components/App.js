dc66ce4cec82e270c24bcea05e5ce2bfc3c3e55e
basem.morkos@redcross.ca <bmorkos@redcross.ca>
2018-06-12 12:58:12 -0400

    add root - header components

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+// functional component
+import React from 'react';
+import Header from './Header';
+
+export default () => {
+	return (
+		<div>
+			<Header />
+			React functional Component
+		</div>
+	);
+};
